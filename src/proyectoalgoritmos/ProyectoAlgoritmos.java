443776067afe7effcd0c214d35e848bc87de341d
Javier Antonio Araya Porras <jantono9916@gmail.com>
2019-09-10 11:19:47 -0600

    avance1

--- src/proyectoalgoritmos/ProyectoAlgoritmos.java
+++ src/proyectoalgoritmos/ProyectoAlgoritmos.java
@@ -5,17 +5,77 @@
  */
 package proyectoalgoritmos;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author rsoli
  */
 public class ProyectoAlgoritmos {
-
-    /**
-     * @param args the command line arguments
-     */
+    
+    public static Grafo g;
+    public static ArrayList<Vertice> verticesLocales;
+    public static ArrayList<Arco> arcosLocales;
+    public static int vertices;
+    public static int arcos;
+    public static int [][] matrizAdy;
+    public static int [][] matriz5 = {{0,1,0,0,0},{1,0,1,1,0},{0,1,0,0,0},{0,1,0,0,1},{0,0,0,1,0}};
+    
+    public static int [][] matriz10 = { {0,0,1,0,0,0,0,0,0,0},{0,0,1,0,0,0,0,0,0,0},
+                                        {1,1,0,1,0,1,0,0,0,0},{0,0,1,0,0,0,0,0,0,0},
+                                        {0,0,0,0,0,1,1,1,0,0},{0,0,1,0,1,0,0,0,1,0},
+                                        {0,0,0,0,1,0,0,0,0,0},{0,0,0,0,1,0,0,0,0,0},
+                                        {0,0,0,0,0,1,0,0,0,1},{0,0,0,0,0,0,0,0,1,0}};
+    
+    public static int [][] matriz20 = { {0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                        {0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                        {0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                        {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                        {1,1,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0},
+                                        {0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
+                                        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0},
+                                        {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+                                        {0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0},
+                                        {0,0,0,0,0,1,0,0,0,0,0,1,1,1,0,0,0,0,0,0},
+                                        {0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                                        {0,0,0,0,0,0,0,0,1,1,0,0,0,0,1,0,0,0,0,0},
+                                        {0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
+                                        {0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
+                                        {0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,1},
+                                        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0},
+                                        {0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0},
+                                        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0},
+                                        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                                        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0},};
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+    }
+    
+    public static void llenarGrafo() {
+        g = new Grafo();
+        verticesLocales = new ArrayList<>();
+        arcosLocales = new ArrayList<>();
+
+        for (int i = 0; i < vertices; i++) {
+            Vertice nuevo = new Vertice(Integer.toString(i), false, 0, false);
+            verticesLocales.add(nuevo);
+            g.addVertice(nuevo);
+        }
+
+        for (int i = 0; i < vertices; i++) {
+            for (int j = 0; j < vertices; j++) {
+                if (matrizAdy[i][j] != 0) {
+                    Vertice origen = g.buscarVerticeGrafo(i);
+                    Vertice destino = g.buscarVerticeGrafo(j);
+                    
+                    if (origen != null && destino != null)
+                        arcosLocales.add(new Arco(origen, destino, matrizAdy[i][j], false));
+                    
+                    origen.addArco(new Arco(origen, destino, matrizAdy[i][j], false));
+                }
+            }
+        }
     }
     
 }
